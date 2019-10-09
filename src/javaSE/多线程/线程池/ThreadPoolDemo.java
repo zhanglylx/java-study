b0f0f43584ef1c1e013fce6841643af5757e3b4c
张连宇 <zhangly@cread.com>
2019-10-09 10:20:30 +0800

    更新

--- src/javaSE/多线程/线程池/ThreadPoolDemo.java
+++ src/javaSE/多线程/线程池/ThreadPoolDemo.java
@@ -2,6 +2,7 @@ package 多线程.线程池;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * 线程池
@@ -9,7 +10,7 @@ import java.util.concurrent.Executors;
  */
 public class ThreadPoolDemo {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
         for (int i = 0; i < 5; i++) {
             Runnable runn = new Runnable() {
                 @Override
@@ -25,8 +26,22 @@ public class ThreadPoolDemo {
 
                 }
             };
-            executorService.execute(runn);
+            threadPoolExecutor.execute(runn);
         }
-        executorService.shutdown();
+        threadPoolExecutor.shutdown();
+
+        System.out.println();
+        int queueSize = threadPoolExecutor.getQueue().size();
+        System.out.println("当前排队线程数：" + queueSize);
+
+        int activeCount = threadPoolExecutor.getActiveCount();
+        System.out.println("当前活动线程数：" + activeCount);
+
+        long completedTaskCount = threadPoolExecutor.getCompletedTaskCount();
+        System.out.println("执行完成线程数：" + completedTaskCount);
+
+        long taskCount = threadPoolExecutor.getTaskCount();
+        System.out.println("总线程数：" + taskCount);
+
     }
 }
